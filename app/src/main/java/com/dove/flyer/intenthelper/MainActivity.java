dd6c2897c0bbf5a284b293ddb4fc654c7bb3d78d
flyer <253311459@qq.com>
2016-12-09 13:39:34 +0800

    add usage

--- app/src/main/java/com/dove/flyer/intenthelper/MainActivity.java
+++ app/src/main/java/com/dove/flyer/intenthelper/MainActivity.java
@@ -1,7 +1,12 @@
 package com.dove.flyer.intenthelper;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.dove.flyer.intenthelper.core.IntentBuilder;
+import com.dove.flyer.intenthelper.core.IntentHelper;
+import com.dove.flyer.intenthelper.core.IntentParser;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +14,21 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new IntentBuilder()
+                .putString("key","")
+                .putInt("key",0)
+                .putFloat("key",0.1f)
+                .putDouble("key",0.1d)
+                .buildIntent();
+        IntentHelper intentHelper = new IntentBuilder()
+                .putString("key","")
+                .putInt("key",0)
+                .putFloat("key",0.1f)
+                .putDouble("key",0.1d)
+                .buildHelper();
+        intentHelper.startActivity(this);
+
+        String value = new IntentParser(getIntent()).getBundle().getString("key");
     }
 }
